9890ea155a6fbaf92df9e41ab08112a9b8cd4631
LuanaAlmeidaMartins <lmartins.comp@gmail.com>
2019-01-22 14:39:36 -0300

    SST and AST

---
+++ src/plugin/ast/DependencyVisitor.java
@@ -0,0 +1,95 @@
+package plugin.ast;
+
+import java.util.ArrayList;
+
+
+import org.eclipse.jdt.core.ICompilationUnit;
+import org.eclipse.jdt.core.IType;
+import org.eclipse.jdt.core.JavaCore;
+import org.eclipse.jdt.core.JavaModelException;
+import org.eclipse.jdt.core.dom.AST;
+import org.eclipse.jdt.core.dom.ASTParser;
+import org.eclipse.jdt.core.dom.ASTVisitor;
+import org.eclipse.jdt.core.dom.ClassInstanceCreation;
+import org.eclipse.jdt.core.dom.CompilationUnit;
+import org.eclipse.jdt.core.dom.FieldDeclaration;
+import org.eclipse.jdt.core.dom.MethodDeclaration;
+import org.eclipse.jdt.core.dom.SingleVariableDeclaration;
+import org.eclipse.jdt.core.dom.TypeDeclaration;
+
+import plugin.persistences.Dependency;
+
+public class DependencyVisitor extends ASTVisitor {
+
+	private CompilationUnit fullClass;
+	private IType clazz;
+	private ArrayList<String> dependencias;
+	ArrayList<Dependency> dp;
+
+	public DependencyVisitor(ICompilationUnit unit) throws JavaModelException {
+		dp = new ArrayList<Dependency>();
+		@SuppressWarnings("deprecation")
+		ASTParser parser = ASTParser.newParser(AST.JLS8);
+		parser.setKind(ASTParser.K_COMPILATION_UNIT);
+		parser.setSource(unit);
+		parser.setCompilerOptions(JavaCore.getOptions());
+		parser.setProject(unit.getJavaProject());
+		parser.setResolveBindings(true);
+		parser.setBindingsRecovery(true);
+
+		this.fullClass = (CompilationUnit) parser.createAST(null);
+		this.fullClass.accept(this);
+	}
+	
+	
+	@Override
+	public boolean visit(TypeDeclaration node) {
+		dependencias = new ArrayList<String>();
+		clazz = (IType) node.resolveBinding().getJavaElement();
+		dp.add(new Dependency(clazz, dependencias));
+		return true;
+	}
+
+	@Override
+	public boolean visit(FieldDeclaration node) {
+		
+		if (!dependencias.contains(node.getType().resolveBinding().getName()) && !node.getType().isPrimitiveType()) {
+				dependencias.add(node.getType().resolveBinding().getName());
+			//	System.out.println("FieldDeclaration "+node.getType().resolveBinding().getName());
+		} 
+
+		return true;
+	}
+
+	@Override
+	public boolean visit(MethodDeclaration node) {
+		for (Object o : node.parameters()) {
+			if (o instanceof SingleVariableDeclaration) {
+				SingleVariableDeclaration svd = (SingleVariableDeclaration) o;
+				if (!dependencias.contains(svd.getType().resolveBinding().getName()) && !svd.getType().isPrimitiveType()) {
+				//	System.out.println("MethodDeclaration "+svd.getType().resolveBinding().getName());
+					dependencias.add(svd.getType().resolveBinding().getName());
+				}
+			}
+		}
+		return true;
+	}
+
+	@Override
+	public boolean visit(ClassInstanceCreation node) {
+		if (!dependencias.contains(node.getType().resolveBinding().getName()) && !node.getType().isPrimitiveType()) {
+		//	System.out.println("ClassInstanceCreation "+node.getType().resolveBinding().getName());
+			dependencias.add(node.getType().resolveBinding().getName());
+		}
+		return true;
+	}
+
+	public IType getClazz() {
+		return clazz;
+	}
+	
+	public ArrayList<Dependency> getDependency() {
+		return dp;
+	}
+
+}
---
+++ src/plugin/persistences/Dependency.java
@@ -0,0 +1,36 @@
+package plugin.persistences;
+
+import java.util.ArrayList;
+import org.eclipse.jdt.core.IType;
+
+public class Dependency {
+	
+	private String newClassName;
+	private IType classe;
+	private ArrayList<String> dependencias;
+	
+	public Dependency(IType classe, ArrayList<String> dependencias){
+		this.classe = classe;
+		this.dependencias = dependencias;
+	}
+
+	public String newClassName() {
+		return newClassName;
+	}
+	
+	public void setNewClassName(String name) {
+		this.newClassName = name;
+	}
+
+	public IType getClasse() {
+		return classe;
+	}
+
+	public ArrayList<String> getDependencias() {
+		return dependencias;
+	}
+	
+	public String getClasseName(){
+		return classe.getElementName();
+	}
+}
